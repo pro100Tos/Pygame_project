7e9a43c382d849cfadcac2a45b876b63f2124234
pro100Tos <118208616+pro100Tos@users.noreply.github.com>
2023-12-22 22:59:17 +0300

    Создание анимации движения

--- main.py
+++ main.py
@@ -20,29 +20,98 @@ def load_image(name, colorkey=None):
 class Hero(pygame.sprite.Sprite):
     def __init__(self, *group):
         super().__init__(*group)
-        self.image = load_image("idle.gif", colorkey=-1)
+        self.image = load_image("idle.png", colorkey=-1)
+        self.animCount = 0
+        self.state = load_image("idle.png", colorkey=-1)
         self.rect = self.image.get_rect()
+        self.run = [load_image("Push1.png", colorkey=-1), load_image("Push2.png", colorkey=-1),
+                    load_image("Push3.png", colorkey=-1), load_image("Push4.png", colorkey=-1),
+                    load_image("Push5.png", colorkey=-1), load_image("Push6.png", colorkey=-1),
+                    load_image("Push7.png", colorkey=-1), load_image("Push8.png", colorkey=-1),
+                    load_image("Push9.png", colorkey=-1), load_image("Push10.png", colorkey=-1),
+                    load_image("Push11.png", colorkey=-1), load_image("Push12.png", colorkey=-1),
+                    load_image("Push13.png", colorkey=-1), load_image("Push14.png", colorkey=-1),
+                    load_image("Push15.png", colorkey=-1), load_image("Push16.png", colorkey=-1),]
+        self.left = False
+        self.right = False
+
+    def draw_run(self):
+        key_pressed_is = pygame.key.get_pressed()
+        self.left = False
+        self.right = False
+
+        if key_pressed_is[pygame.K_LEFT]:
+            self.rect.x -= 5
+            self.left = True
+        if key_pressed_is[pygame.K_RIGHT]:
+            self.rect.x += 5
+            self.right = True
+        if key_pressed_is[pygame.K_UP]:
+            self.rect.y -= 5
+        if key_pressed_is[pygame.K_DOWN]:
+            self.rect.y += 5
+
+    def update(self):
+        if self.animCount + 1 >= 32:
+            self.animCount = 0
+        if self.left:
+            self.image = self.run[self.animCount // 2]
+            self.image = pygame.transform.scale(self.image, (60, 70))
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.animCount += 1
+        elif self.right:
+            self.image = self.run[self.animCount // 2]
+            self.image = pygame.transform.scale(self.image, (60, 70))
+            self.animCount += 1
+        else:
+            self.image = self.state
+            self.image = pygame.transform.scale(self.image, (60, 70))
 
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, *group, hero):
         super().__init__(*group)
         self.image = load_image("goblin_run1.png", colorkey=-1)
-        self.image = pygame.transform.scale(self.image, (40, 50))
+        self.animCount = 0
+        self.state = load_image("goblin_run1.png", colorkey=-1)
+        self.run = [load_image("goblin_run1.png", colorkey=-1), load_image("goblin run2.png", colorkey=-1),
+                    load_image("goblin run3.png", colorkey=-1), load_image("goblin run4.png", colorkey=-1),
+                    load_image("goblin run5.png", colorkey=-1), load_image("goblin run6.png", colorkey=-1)]
+        self.image = pygame.transform.scale(self.image, (60, 70))
         self.rect = self.image.get_rect()
         self.rect.x = 600
         self.rect.y = 600
         self.hero = hero
+        self.left = False
+        self.right = False
 
     def update(self):
+        self.left = False
+        self.right = False
         if self.hero.rect.x > self.rect.x:
-            self.rect.x += 5
+            self.rect.x += 2
+            self.right = True
         if self.hero.rect.x < self.rect.x:
-            self.rect.x -= 5
+            self.rect.x -= 2
+            self.left = True
         if self.hero.rect.y > self.rect.y:
-            self.rect.y += 5
+            self.rect.y += 2
         if self.hero.rect.y < self.rect.y:
-            self.rect.y -= 5
+            self.rect.y -= 2
+        if self.animCount + 1 >= 30:
+            self.animCount = 0
+        if self.left:
+            self.image = self.run[self.animCount // 5]
+            self.image = pygame.transform.scale(self.image, (60, 70))
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.animCount += 1
+        elif self.right:
+            self.image = self.run[self.animCount // 5]
+            self.image = pygame.transform.scale(self.image, (60, 70))
+            self.animCount += 1
+        else:
+            self.image = self.state
+            self.image = pygame.transform.scale(self.image, (60, 70))
 
 
 if __name__ == '__main__':
@@ -62,7 +131,7 @@ if __name__ == '__main__':
     all_sprites.add(enemy)
 
     while running:
-        clock.tick(60)
+        clock.tick(32)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -71,21 +140,13 @@ if __name__ == '__main__':
             if event.type == pygame.USEREVENT:
                 all_sprites.add(Enemy(hero=hero))
 
-        key_pressed_is = pygame.key.get_pressed()
-
-        if key_pressed_is[pygame.K_LEFT]:
-            hero.rect.x -= 10
-        if key_pressed_is[pygame.K_RIGHT]:
-            hero.rect.x += 10
-        if key_pressed_is[pygame.K_UP]:
-            hero.rect.y -= 10
-        if key_pressed_is[pygame.K_DOWN]:
-            hero.rect.y += 10
-
         screen.fill((0, 0, 0))
 
         all_sprites.draw(screen)
+
+        hero.draw_run()
         all_sprites.update()
 
         pygame.display.flip()
+
     pygame.quit()
