4bdb195ccff3cf3d822a7e8e840597b6d52960e0
pro100Tos <118208616+pro100Tos@users.noreply.github.com>
2023-12-25 17:11:47 +0300

    Объединение main и Tile (персонажи проходят сквозь текстуры)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame, os, sys
 
+size_player = p_width, p_height = 30, 40
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('images_data', name)
@@ -15,6 +17,50 @@ def load_image(name, colorkey=None):
     elif colorkey is None:
         image = image.convert_alpha()
     return image
+
+
+def load_image_data_tile(name, colorkey=None):
+    fullname = os.path.join('data_tile', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def load_level(filename):
+    filename = "data_tile/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def generate_level(level):
+    x, y = None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                pass
+            elif level[y][x] == '#':
+                Wall('wall', x, y)
+            elif level[y][x] == '*':
+                Ladder('ladder', x, y)
+            elif level[y][x] == '%':
+                Block('block', x, y)
+    return x, y
 
 
 class Hero(pygame.sprite.Sprite):
@@ -41,31 +87,31 @@ class Hero(pygame.sprite.Sprite):
         self.right = False
 
         if key_pressed_is[pygame.K_LEFT]:
-            self.rect.x -= 5
+            self.rect.x -= 3
             self.left = True
         if key_pressed_is[pygame.K_RIGHT]:
-            self.rect.x += 5
+            self.rect.x += 3
             self.right = True
         if key_pressed_is[pygame.K_UP]:
-            self.rect.y -= 5
+            self.rect.y -= 3
         if key_pressed_is[pygame.K_DOWN]:
-            self.rect.y += 5
+            self.rect.y += 3
 
     def update(self):
         if self.animCount + 1 >= 32:
             self.animCount = 0
         if self.left:
             self.image = self.run[self.animCount // 2]
-            self.image = pygame.transform.scale(self.image, (60, 70))
+            self.image = pygame.transform.scale(self.image, size_player)
             self.image = pygame.transform.flip(self.image, True, False)
             self.animCount += 1
         elif self.right:
             self.image = self.run[self.animCount // 2]
-            self.image = pygame.transform.scale(self.image, (60, 70))
+            self.image = pygame.transform.scale(self.image, size_player)
             self.animCount += 1
         else:
             self.image = self.state
-            self.image = pygame.transform.scale(self.image, (60, 70))
+            self.image = pygame.transform.scale(self.image, size_player)
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -77,7 +123,7 @@ class Enemy(pygame.sprite.Sprite):
         self.run = [load_image("goblin_run1.png", colorkey=-1), load_image("goblin run2.png", colorkey=-1),
                     load_image("goblin run3.png", colorkey=-1), load_image("goblin run4.png", colorkey=-1),
                     load_image("goblin run5.png", colorkey=-1), load_image("goblin run6.png", colorkey=-1)]
-        self.image = pygame.transform.scale(self.image, (60, 70))
+        self.image = pygame.transform.scale(self.image, size_player)
         self.rect = self.image.get_rect()
         self.rect.x = 600
         self.rect.y = 600
@@ -89,35 +135,58 @@ class Enemy(pygame.sprite.Sprite):
         self.left = False
         self.right = False
         if self.hero.rect.x > self.rect.x:
-            self.rect.x += 2
+            self.rect.x += 1
             self.right = True
         if self.hero.rect.x < self.rect.x:
-            self.rect.x -= 2
+            self.rect.x -= 1
             self.left = True
         if self.hero.rect.y > self.rect.y:
-            self.rect.y += 2
+            self.rect.y += 1
         if self.hero.rect.y < self.rect.y:
-            self.rect.y -= 2
+            self.rect.y -= 1
         if self.animCount + 1 >= 30:
             self.animCount = 0
         if self.left:
             self.image = self.run[self.animCount // 5]
-            self.image = pygame.transform.scale(self.image, (60, 70))
+            self.image = pygame.transform.scale(self.image, size_player)
             self.image = pygame.transform.flip(self.image, True, False)
             self.animCount += 1
         elif self.right:
             self.image = self.run[self.animCount // 5]
-            self.image = pygame.transform.scale(self.image, (60, 70))
+            self.image = pygame.transform.scale(self.image, size_player)
             self.animCount += 1
         else:
             self.image = self.state
-            self.image = pygame.transform.scale(self.image, (60, 70))
+            self.image = pygame.transform.scale(self.image, size_player)
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Wall(Tile):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tile_type, pos_x, pos_y)
+
+
+class Ladder(Tile):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tile_type, pos_x, pos_y)
+
+
+class Block(Tile):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tile_type, pos_x, pos_y)
 
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('runner')
-    size = width, height = 1200, 800
+    size = width, height = 840, 600
     screen = pygame.display.set_mode(size)
 
     running = True
@@ -125,17 +194,30 @@ if __name__ == '__main__':
     pygame.time.set_timer(pygame.USEREVENT, 5000)
 
     all_sprites = pygame.sprite.Group()
+    tiles_group = pygame.sprite.Group()
     hero = Hero()
     all_sprites.add(hero)
     enemy = Enemy(hero=hero)
     all_sprites.add(enemy)
+
+    tile_images = {
+        'wall': load_image_data_tile('brick_2.png'),
+        'ladder': load_image_data_tile('ladder.png'),
+        'block': load_image_data_tile('block.png')
+    }
+
+    tile_width, tile_height = 30, 25
+
+    title = True
+    level_x, level_y = generate_level(load_level('map.txt'))
+    tiles_group.draw(screen)
 
     while running:
         clock.tick(32)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                terminate()
 
             if event.type == pygame.USEREVENT:
                 all_sprites.add(Enemy(hero=hero))
